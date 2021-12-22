f50e6e5a254030e2be8d57ecd0a5fdfe611e56ed
zyar3k <a.zygan@gmail.com>
2021-12-22 19:34:51 +0100

    Add get all job by user

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -3,9 +3,8 @@ const { BadRequestError, NotFoundError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
 
 const getAllJobs = async (req, res) => {
-  res.send("get all jobs");
-  //   const jobs = await Job.find();
-  //   res.send(jobs);
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 
 const getJob = async (req, res) => {
@@ -15,8 +14,6 @@ const getJob = async (req, res) => {
 };
 
 const createJob = async (req, res) => {
-  // res.json(req.body);
-  // create user id
   req.body.createdBy = req.user.userId;
   const job = await Job.create(req.body);
   res.status(StatusCodes.CREATED).json({ job });
