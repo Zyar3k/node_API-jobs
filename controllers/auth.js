0fd76c82f124a4114177f7904e802c8ae6674a70
zyar3k <a.zygan@gmail.com>
2021-12-22 16:54:53 +0100

    Finish register logic in model

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,10 @@
 const User = require("../models/User");
-const jwt = require("jsonwebtoken");
 const { StatusCodes } = require("http-status-codes");
 
 const register = async (req, res) => {
   const newUser = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json(newUser);
+  const token = newUser.createJWT();
+  res.status(StatusCodes.CREATED).json({ user: { name: newUser.name }, token });
 };
 
 const login = async (req, res) => {
