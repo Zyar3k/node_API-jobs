f8fac4b2ae4f8ee6658cdca877ac883d77aff966
zyar3k <a.zygan@gmail.com>
2021-12-22 17:06:05 +0100

    Add vars to env

--- models/User.js
+++ models/User.js
@@ -36,9 +36,13 @@ UserSchema.methods.getName = function () {
 };
 
 UserSchema.methods.createJWT = function () {
-  return jwt.sign({ userId: this._id, name: this.name }, "jwtSecret", {
-    expiresIn: "30d",
-  });
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    }
+  );
 };
 
 module.exports = mongoose.model("User", UserSchema);
