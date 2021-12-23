4f6dbd7003bf119c14eb2ffddee74a31e280eb5a
zyar3k <a.zygan@gmail.com>
2021-12-23 16:48:25 +0100

    Improve answer for cast error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -10,11 +10,22 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   // if (err instanceof CustomAPIError) {
   //   return res.status(err.statusCode).json({ msg: err.message });
   // }
+  if (err.name === "ValidationError") {
+    customError.msg = Object.values(err.errors)
+      .map((val) => val.message)
+      .join(", ");
+    customError.statusCode = 400;
+  }
+
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value entered for ${Object.keys(
       err.keyValue
     )} field, please enter another value`;
     customError.statusCode = 400;
+  }
+  if (err.name === "CastError") {
+    customError.msg = `No item found with ${err.value}`;
+    customError.statusCode = 404;
   }
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
   return res.status(customError.statusCode).json({ msg: customError.msg });
